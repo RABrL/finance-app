b686e47dd10ffbd2881b72fcef4de021c6c18ad8
RABrL <robin_brito@hotmail.com>
2024-05-24 17:58:50 -0500

    Add POST endpoint for /accounts and start with sheet component for the create new account form

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,23 +1,49 @@
 import { Hono } from 'hono'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
-
-import { db } from '@/db/drizzle'
-import { accounts } from '@/db/schema'
+import { zValidator } from '@hono/zod-validator'
 import { eq } from 'drizzle-orm'
 
-const app = new Hono().get('/', clerkMiddleware(), async (c) => {
-  const auth = getAuth(c)
+import { db } from '@/db/drizzle'
+import { accounts, insertAccountSchema } from '@/db/schema'
 
-  if (!auth?.userId) {
-    return c.json({ error: 'Unauthorized' }, 401)
-  }
+const app = new Hono()
+  .get('/', clerkMiddleware(), async (c) => {
+    const auth = getAuth(c)
 
-  const data = await db
-    .select({ id: accounts.id, name: accounts.name })
-    .from(accounts)
-    .where(eq(accounts.userId, auth.userId))
+    if (!auth?.userId) {
+      return c.json({ error: 'Unauthorized' }, 401)
+    }
 
-  return c.json({ data })
-})
+    const data = await db
+      .select({ id: accounts.id, name: accounts.name })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId))
+
+    return c.json({ data })
+  })
+  .post(
+    '/',
+    clerkMiddleware(),
+    zValidator('json', insertAccountSchema.pick({ name: true })),
+    async (c) => {
+      const auth = getAuth(c)
+
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized' }, 401)
+      }
+
+      const values = c.req.valid('json')
+
+      const [data] = await db
+        .insert(accounts)
+        .values({
+          userId: auth.userId,
+          ...values
+        })
+        .returning()
+
+      return c.json({ data })
+    }
+  )
 
 export default app
