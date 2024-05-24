e0ad98dc3e64fd4d777fbfcaee46b41174867066
RABrL <robin_brito@hotmail.com>
2024-05-24 16:58:26 -0500

    Add /accounts endpoint and create a service for getAccounts

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,3 @@
-
 export default function Home() {
-  return (<></>)
+  return <div>Home Page</div>
 }
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,33 +1,15 @@
 import { Hono } from 'hono'
-import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator'
 import { handle } from 'hono/vercel'
+
+import accounts from './accounts'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app
-  .get('/hello', (c) => {
-    return c.json({
-      message: 'Hello Next.js!'
-    })
-  })
-  .get(
-    '/hello/:name',
-    zValidator(
-      'param',
-      z.object({
-        name: z.number()
-      })
-    ),
-    (c) => {
-      const {name} = c.req.valid('param')
-      return c.json({
-        message: `Hello ${name}!`
-      })
-    }
-  )
+const routes = app.route('/accounts', accounts)
 
 export const GET = handle(app)
 export const POST = handle(app)
+
+export type AppType = typeof routes
