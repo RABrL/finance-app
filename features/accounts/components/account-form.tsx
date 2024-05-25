0bb5f8ee178dcd62f8a25c0f62cb96aa01c8cb1c
RABrL <robin_brito@hotmail.com>
2024-05-25 17:09:23 -0500

    chore: Add Toaster component for displaying notifications and finish the create account feature

---
+++ features/accounts/components/account-form.tsx
@@ -0,0 +1,90 @@
+import type { z } from 'zod'
+import { Trash } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { insertAccountSchema } from '@/db/schema'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel
+} from '@/components/ui/form'
+
+const formSchema = insertAccountSchema.pick({
+  name: true
+})
+
+type FormValues = z.infer<typeof formSchema>
+
+type Props = {
+  id?: string
+  defaultValues?: FormValues
+  onSubmit: (values: FormValues) => void
+  onDelete?: () => void
+  disabled?: boolean
+}
+
+export const AccountForm = ({
+  id,
+  defaultValues,
+  disabled,
+  onDelete,
+  onSubmit
+}: Props) => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues
+  })
+
+  const handleSubmit = (values: FormValues) => {
+    onSubmit(values)
+  }
+
+  const handleDelete = () => {
+    onDelete?.()
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="space-y-4 pt-4"
+      >
+        <FormField
+          name="name"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input
+                  disabled={disabled}
+                  placeholder="e.g. Cash, bank, Credit, Card"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button disabled={disabled} className="w-full">
+          {id ? 'Save changes' : 'Create account'}
+        </Button>
+        {!!id && (
+          <Button
+            type="button"
+            className='w-full'
+            disabled={disabled}
+            variant='outline'
+            onClick={handleDelete}
+          >
+            <Trash className='size-4 mr-2' />
+            Delete Account
+          </Button>
+        )}
+      </form>
+    </Form>
+  )
+}
--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,3 +1,9 @@
+import type { z } from 'zod'
+
+import { useNewAccount } from '@/features/accounts/hooks/use-new-account'
+import { AccountForm } from '@/features/accounts/components/account-form'
+
+import { insertAccountSchema } from '@/db/schema'
 import {
   Sheet,
   SheetContent,
@@ -5,10 +11,26 @@ import {
   SheetHeader,
   SheetTitle
 } from '@/components/ui/sheet'
-import { useNewAccount } from '@/features/accounts/hooks/use-new-account'
+import { useCreateAccount } from '@/features/accounts/api/use-create-account'
+
+const formSchema = insertAccountSchema.pick({
+  name: true
+})
+
+type FormValues = z.infer<typeof formSchema>
 
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount()
+  const mutation = useCreateAccount()
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values,{
+      onSuccess: () => {
+        onClose()
+      }
+    })
+  }
+
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
       <SheetContent className="space-y-4">
@@ -18,6 +40,7 @@ export const NewAccountSheet = () => {
             Create a new account to track your transactions.
           </SheetDescription>
         </SheetHeader>
+        <AccountForm onSubmit={onSubmit} disabled={mutation.isPending} defaultValues={{ name: '' }} />
       </SheetContent>
     </Sheet>
   )
