b16f83e7c56f82ca97df7417529d7252a4c53683
RABrL <robin_brito@hotmail.com>
2024-05-25 18:21:26 -0500

    Create data table componet

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,11 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { useNewAccount } from '@/features/accounts/hooks/use-new-account'
 
 export default function Home() {
-  const { onOpen } = useNewAccount()
   return (
     <div>
-      <Button onClick={onOpen}>Open</Button>
+      <Button>Open</Button>
     </div>
   )
 }
