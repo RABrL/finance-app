5141d9fddc1e1186fb1576ca62b9e6c64d1b3ff7
RABrL <robin_brito@hotmail.com>
2024-05-25 20:09:37 -0500

    chore: Refactor table-loading component for improved performance and readability

--- components/table-loading.tsx
+++ components/table-loading.tsx
@@ -5,14 +5,14 @@ export const TableLoading = () => {
     <>
       <Skeleton className="h-11 max-w-sm my-4" />
       <Skeleton className="h-[300px] w-full" />
-      <div className="flex items-center">
-        <div className="flex items-center gap-x-2 flex-1 text-sm text-muted-foreground">
-          <Skeleton className="size-4 rounded-sm inline-block" /> of{' '}
-          <Skeleton className="size-4 rounded-sm inline-block" /> row(s)
+      <div className="flex items-center w-full">
+        <div className="flex-1 text-sm text-muted-foreground">
+          <Skeleton className="h-5 w-3 -my-1 rounded-sm inline-block" /> of{' '}
+          <Skeleton className="h-5 w-3 -my-1 rounded-sm inline-block" /> row(s)
           selected.
         </div>
         <div className="flex items-center justify-end space-x-2 py-4">
-          <Skeleton className="h-8 w-24" />
+          <Skeleton className="h-8 w-20" />
           <Skeleton className="h-8 w-16" />
         </div>
       </div>
