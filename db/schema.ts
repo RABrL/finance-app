b8ae52f1c70715abedebdfbdc3ebec41f2a00479
RABrL <robin_brito@hotmail.com>
2024-05-25 23:43:58 -0500

    Add new transaction table to the schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,7 @@
-import { pgTable, text, uuid } from 'drizzle-orm/pg-core'
+import { z } from 'zod'
 import { createInsertSchema } from 'drizzle-zod'
+import { relations } from 'drizzle-orm'
+import { integer, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 
 export const accounts = pgTable('accounts', {
   id: uuid('id').primaryKey().defaultRandom().notNull(),
@@ -7,6 +9,10 @@ export const accounts = pgTable('accounts', {
   name: text('name').notNull(),
   userId: text('user_id').notNull()
 })
+
+export const accountRelations = relations(accounts, ({ many }) => ({
+  transactions: many(transactions)
+}))
 
 export const insertAccountSchema = createInsertSchema(accounts)
 
@@ -17,4 +23,39 @@ export const categories = pgTable('categories', {
   userId: text('user_id').notNull()
 })
 
+export const categoryRelations = relations(categories, ({ many }) => ({
+  transactions: many(transactions)
+}))
+
 export const insertCategorySchema = createInsertSchema(categories)
+
+export const transactions = pgTable('transactions', {
+  id: uuid('id').primaryKey().defaultRandom().notNull(),
+  amount: integer('amount').notNull(),
+  payee: text('payee').notNull(),
+  notes: text('notes'),
+  date: timestamp('date', { mode: 'date' }).notNull(),
+  accountId: uuid('account_id')
+    .references(() => accounts.id, {
+      onDelete: 'cascade'
+    })
+    .notNull(),
+  categoryId: uuid('category_id').references(() => categories.id, {
+    onDelete: 'set null'
+  })
+})
+
+export const transactionRelations = relations(transactions, ({ one }) => ({
+  account: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id]
+  }),
+  category: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id]
+  })
+}))
+
+export const insertTransactionSchema = createInsertSchema(transactions, {
+  date: z.coerce.date()
+})
