29e886ce68e95905f8750aa1fad837c0d22ca4d9
RABrL <robin_brito@hotmail.com>
2024-05-25 22:22:10 -0500

    organice imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,17 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import { QueryProvider } from '@/providers/query-provider'
+import { SheetProvider } from '@/providers/sheet-provider'
+
+import { Toaster } from '@/components/ui/sonner'
 
 import './globals.css'
-import { SheetProvider } from '@/providers/sheet-provider'
-import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Finance App',
-  description: 'A finance app for personal use and for learning'
+  description: 'A finance app for personal use and learning'
 }
 
 export default function RootLayout({
