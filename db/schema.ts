ab8164ff9dde2a5930a64ab7d2b0f106f79ab413
RABrL <robin_brito@hotmail.com>
2024-05-25 22:54:52 -0500

    feat: Add categories table to database schema

--- db/schema.ts
+++ db/schema.ts
@@ -9,3 +9,12 @@ export const accounts = pgTable('accounts', {
 })
 
 export const insertAccountSchema = createInsertSchema(accounts)
+
+export const categories = pgTable('categories', {
+  id: uuid('id').primaryKey().defaultRandom().notNull(),
+  plaidId: text('plaid_id'),
+  name: text('name').notNull(),
+  userId: text('user_id').notNull()
+})
+
+export const insertCategorySchema = createInsertSchema(categories)
