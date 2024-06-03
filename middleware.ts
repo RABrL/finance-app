4b828a2dc600a8bbe9364f9740d033990537ba03
RABrL <robin_brito@hotmail.com>
2024-06-03 13:57:22 -0500

    refactor: Update middleware to protect public routes instead of checking for protected routes

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,10 @@ import { NextResponse } from 'next/server'
 import { clerkMiddleware } from '@clerk/nextjs/server'
 import { createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/'])
+const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up']);
 
 export default clerkMiddleware((auth, request) => {
-  if (isProtectedRoute(request)) {
+  if (!isPublicRoute(request)) {
     auth().protect()
   }
 
