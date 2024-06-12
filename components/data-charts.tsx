42dd7f258a5cd4e1ffca4467cdc0fede0a384f12
RABrL <robin_brito@hotmail.com>
2024-06-11 22:32:38 -0500

    feat: Add loading state to DataCharts component

--- components/data-charts.tsx
+++ components/data-charts.tsx
@@ -2,16 +2,24 @@
 
 import { useGetSummary } from '@/features/summary/api/use-get-summary'
 
-import { Chart } from '@/components/chart'
+import { Chart, ChartLoading } from '@/components/chart'
+import { SpendingPie, SpendingPieLoading } from '@/components/spending-pie'
 
 export const DataCharts = () => {
   const { data, isLoading } = useGetSummary()
 
-  if (isLoading) return <div>Loading...</div>
-
-  return <div className='grid grid-cols-1 lg:grid-cols-6 gap-8'>
-    <div className='col-span-1 lg:col-span-3 xl:col-span-4'>
-      <Chart data={data?.days}/>
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-6 gap-8">
+      <div className="col-span-1 lg:col-span-3 xl:col-span-4">
+        {isLoading ? <ChartLoading /> : <Chart data={data?.days} />}
+      </div>
+      <div className="col-span-1 lg:col-span-3 xl:col-span-2">
+        {isLoading ? (
+          <SpendingPieLoading />
+        ) : (
+          <SpendingPie data={data?.categories} />
+        )}
+      </div>
     </div>
-  </div>
+  )
 }
