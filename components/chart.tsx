ec841cce5bbf262d26932545c5a082d8265beba1
RABrL <robin_brito@hotmail.com>
2024-06-11 22:31:49 -0500

    feat: Add ChartLoading component for displaying loading state in charts

--- components/chart.tsx
+++ components/chart.tsx
@@ -1,6 +1,13 @@
 import { useState } from 'react'
-import { AreaChart, BarChart3, FileSearch, LineChart } from 'lucide-react'
+import {
+  AreaChart,
+  BarChart3,
+  FileSearch,
+  LineChart,
+  Loader2
+} from 'lucide-react'
 
+import { Skeleton } from '@/components/ui/skeleton'
 import { AreaVariant } from '@/components/area-variant'
 import { BarVariant } from '@/components/bar-variant'
 import { LineVariant } from '@/components/line-variant'
@@ -73,6 +80,22 @@ export const Chart = ({ data = [] }: Props) => {
             {chartType === 'line' && <LineVariant data={data} />}
           </>
         )}
+      </CardContent>
+    </Card>
+  )
+}
+
+export const ChartLoading = () => {
+  return (
+    <Card className="border-none drop-shadow-sm">
+      <CardHeader className="flex space-y-2 lg:space-y-0 lg:flex-row lg:items-center justify-between">
+        <CardTitle className="text-xl line-clamp-1">Transactions</CardTitle>
+        <Skeleton className="h-8 lg:w-[120px] w-full" />
+      </CardHeader>
+      <CardContent>
+        <div className="h-[350px] w-full flex items-center justify-center">
+          <Loader2 className="size-6 text-slate-300 animate-spin" />
+        </div>
       </CardContent>
     </Card>
   )
