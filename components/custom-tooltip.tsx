7c9cf7dcb41945ac36d2a389ee69b82ecba1c08c
RABrL <robin_brito@hotmail.com>
2024-06-11 22:32:31 -0500

    Refactor CustomTooltip component to remove unused import

--- components/custom-tooltip.tsx
+++ components/custom-tooltip.tsx
@@ -2,7 +2,6 @@ import { format } from 'date-fns'
 
 import { formatCurrency } from '@/lib/utils'
 import { Separator } from '@/components/ui/separator'
-import { Tooltip } from './ui/tooltip'
 
 export const CustomTooltip = ({ active, payload }: any) => {
   if (!active) return null
